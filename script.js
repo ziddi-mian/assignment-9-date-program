131c3bdb83b99df5c43f2feb6453b8c61ae3ca1f
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-19 11:08:10 +0500

    program 3

--- script.js
+++ script.js
@@ -119,19 +119,19 @@ console.log(compare_dates(new Date('12/18/2021 00:01'), new Date('12/18/2021 00:
 console.log(compare_dates(new Date('12/18/2021 00:00'), new Date('12/18/2021 00:01')));
 
 
-// // programk 6
+// programk 6
 
-// // Write a JavaScript function to add specified minutes to a Date object.   
+// Write a JavaScript function to add specified minutes to a Date object.   
 
-// // Test Data :
-// // console.log(add_minutes(new Date(2014,10,2), 30).toString());
-// // Output :
-// // "Sun Nov 02 2014 00:30:00 GMT+0530 (India Standard Time)"
+// Test Data :
+// console.log(add_minutes(new Date(2014,10,2), 30).toString());
+// Output :
+// "Sun Nov 02 2014 00:30:00 GMT+0530 (India Standard Time)"
 
-// var add_minutes =  function (dt, minutes) {
-//     return new Date(dt.getTime() + minutes*60000);
-// }
-// console.log(add_minutes(new Date(2020,11,18), 30).toString());
+var add_minutes =  function (dt, minutes) {
+    return new Date(dt.getTime() + minutes*60000);
+}
+console.log(add_minutes(new Date(2020,11,18), 30).toString());
 
 
 // // program 7 
