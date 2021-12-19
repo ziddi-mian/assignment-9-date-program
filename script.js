05d4659575ff9e5692deb9503c3f92009e394dd9
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-19 11:09:03 +0500

    program 4

--- script.js
+++ script.js
@@ -134,32 +134,32 @@ var add_minutes =  function (dt, minutes) {
 console.log(add_minutes(new Date(2020,11,18), 30).toString());
 
 
-// // program 7 
+// program 7 
 
-// // Write a JavaScript function to test whether a date is a weekend. Go to the editor
+// Write a JavaScript function to test whether a date is a weekend. Go to the editor
 
-// // Note : Use standard Saturday/Sunday definition of a weekend.
-// // Test Data :
-// // console.log(is_weekend('Nov 15, 2014'));
-// // console.log(is_weekend('Nov 16, 2014'));
-// // console.log(is_weekend('Nov 17, 2014'));
-// // Output :
-// // "weekend"
-// // "weekend"
-// // undefined
-
-// var is_weekend =  function(date1){
-//     var dt = new Date(date1);
-     
-//     if(dt.getDay() == 6 || dt.getDay() == 0)
-//        {
-//         return "weekend";
-//         } 
-// }
-
+// Note : Use standard Saturday/Sunday definition of a weekend.
+// Test Data :
 // console.log(is_weekend('Nov 15, 2014'));
 // console.log(is_weekend('Nov 16, 2014'));
 // console.log(is_weekend('Nov 17, 2014'));
+// Output :
+// "weekend"
+// "weekend"
+// undefined
+
+var is_weekend =  function(date1){
+    var dt = new Date(date1);
+     
+    if(dt.getDay() == 6 || dt.getDay() == 0)
+       {
+        return "weekend";
+        } 
+}
+
+console.log(is_weekend('Nov 15, 2014'));
+console.log(is_weekend('Nov 16, 2014'));
+console.log(is_weekend('Nov 17, 2014'));
 
 
 // // program 8
