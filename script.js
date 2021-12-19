a4ebc64d3ddc0f6c8b5e0958b02f9d8267f42071
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-19 11:10:44 +0500

    program 5

--- script.js
+++ script.js
@@ -162,23 +162,23 @@ console.log(is_weekend('Nov 16, 2014'));
 console.log(is_weekend('Nov 17, 2014'));
 
 
-// // program 8
-// // Write a JavaScript function to get difference between two dates in days. Go to the editor
+// program 8
+// Write a JavaScript function to get difference between two dates in days. Go to the editor
 
-// // Test Data :
-// // console.log(date_diff_indays('04/02/2014', '11/04/2014'));
-// // console.log(date_diff_indays('12/02/2014', '11/04/2014'));
-// // Output :
-// // 216
-// // -28
+// Test Data :
+// console.log(date_diff_indays('04/02/2014', '11/04/2014'));
+// console.log(date_diff_indays('12/02/2014', '11/04/2014'));
+// Output :
+// 216
+// -28
 
-// var date_diff_indays = function(date1, date2) {
-//     dt1 = new Date(date1);
-//     dt2 = new Date(date2);
-//     return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
-//     }
-//     console.log(date_diff_indays('04/02/2014', '11/04/2014'));
-//     console.log(date_diff_indays('12/02/2014', '11/04/2014'));
+var date_diff_indays = function(date1, date2) {
+    dt1 = new Date(date1);
+    dt2 = new Date(date2);
+    return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
+    }
+    console.log(date_diff_indays('04/02/2014', '11/04/2014'));
+    console.log(date_diff_indays('12/02/2014', '11/04/2014'));
 
 
 // //     program 9
